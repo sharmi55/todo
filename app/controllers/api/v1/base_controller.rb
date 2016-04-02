fac3644728fecd68745b3700401922c49b8250a8
Sharmi <rai.sharmistha@gmail.com>
2016-04-02 16:43:46 +0200

    create BaseController and define private method

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,10 @@
+class ApiController < ApplicationController
+  skip_before_Action :verify_authenticity_token
+
+
+private
+  def authenticated?
+    authenticate_or_request_with_http_basic {|username, password| User.where(username: username, password: password).present? }
+  end
+
+end
