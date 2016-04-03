2076cdb246cb7e381a706f83535f2eb5ccd676be
Sharmi <rai.sharmistha@gmail.com>
2016-04-03 13:57:13 +0200

    generate base/api controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,71 +4,14 @@ Rails.application.routes.draw do
 
   root 'lists#index'
 
-resources :items
+    resources :items
+#    resources :users
+    resources :lists
 
-resources :users
-
-resources :lists
-
-namespace :api do
-  namespace :v1 do
-    resources :users, only: [:index, :show, :create, :update]
-    resources :lists, only: [:index, :show]
-  end
-end
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    namespace :api, defaults: { format: :json } do
+#      namespace :v1 do
+        resources :users
+#        resources :lists, only: [:index, :show]
+      end
+#    end
 end
