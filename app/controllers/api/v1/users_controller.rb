6c46546a230d21fa9a22a769fef7098c846aae1c
Sharmi <rai.sharmistha@gmail.com>
2016-04-03 14:05:38 +0200

    generate users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,29 +1,20 @@
-class Api::V1::UsersController < Api::V1
+class Api::UsersController < ApiController
+
+  before_action :authenticated?
 
   def index
     users = User.all
-    render json: users.to_json
+    render json: users, each_serializer: UserSerializer
   end
 
-  def show
-    user = User.find(params[:id])
-    render json: user.to_json
-  end
-
-  def index
-    @users = User.all
-    render json: users, each_serializer: InsecureUserSerializer
-  end
-
-  def new
-    @user = User.new
-  end
-
+#  def show
+#    user = User.find(params[:id])
+#    render json: user.to_json
+#  end
+end
 
 #  def update
 #  end
 
 #  def create
 #  end
-
-end
