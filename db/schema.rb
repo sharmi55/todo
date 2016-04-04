3d46819b4575e05709d430c0779e60c8d1dad2c0
Sharmi <rai.sharmistha@gmail.com>
2016-04-04 19:42:45 +0200

    User.username attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403150845) do
+ActiveRecord::Schema.define(version: 20160404173428) do
 
   create_table "items", force: :cascade do |t|
     t.text     "body"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20160403150845) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "auth_token"
+    t.string   "username"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
