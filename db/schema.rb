7d0d024d1aa0a7a89c2593eea05d5de6ec99a1af
Sharmi <rai.sharmistha@gmail.com>
2016-04-06 14:39:52 +0200

    create user_id attribute in lists table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404190441) do
+ActiveRecord::Schema.define(version: 20160406122734) do
 
   create_table "items", force: :cascade do |t|
     t.text     "body"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160404190441) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
